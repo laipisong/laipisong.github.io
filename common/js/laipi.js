369d455e460928d1ed481291903b7067dc8550cc
赖皮 <1761817111@qq.com>
2017-05-02 18:46:44 +0800

    Add files via upload

---
+++ common/js/laipi.js
@@ -0,0 +1,14 @@
+document.writeln("<link href=\'http://laipi.win/favicon.ico\' rel=\'icon\'>");
+document.writeln("<meta name=\'viewport\' content=\'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0\' />");
+document.writeln("<meta content=\'yes\' name=\'apple-mobile-web-app-capable\' />");
+document.writeln("<meta content=\'black\' name=\'apple-mobile-web-app-status-bar-style\' />");
+document.writeln("<meta content=\'telephone=no\' name=\'format-detection\' />");
+
+
+
+
+//<link href="http://laipi.win/favicon.ico" rel="icon">
+//<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+//<meta content="yes" name="apple-mobile-web-app-capable" />
+//<meta content="black" name="apple-mobile-web-app-status-bar-style" />
+//<meta content="telephone=no" name="format-detection" />
